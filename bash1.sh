dbfde85f0888db8473e37f03bc40715356d7b1c0
mcpatral <kelvinosaigbovo112@gmail.com>
2023-08-19 00:02:11 +0100

    reading user input script

---
+++ bash1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Discription: Reading user input
+#maintainer: Kelvin Osaigbovvo
+
+read -p 'What is your name: ' name
+read -s -p 'Enter your password: ' pass
+
+echo -e "You are welcome $name"
